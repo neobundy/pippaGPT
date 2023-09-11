17698f1c0f693e8477dab0dce74b14c0396dab27
Wankyu Choi <wankyuchoi@gmail.com>
2023-09-11 14:18:01 +0900

    v1.0b

--- settings.py
+++ settings.py
@@ -14,7 +14,7 @@ LOG_FILE = "pippa.log"
 
 # Project
 PROJECT_TITLE = f"Pippa - Personalized, Ingesting, Persistent, Polymorphic, Adaptive GPT Chatbot"
-VERSION = "1.0a"
+VERSION = "1.0b"
 
 # ZEP Long-term Memory
 ZEP_API_URL = settings_private.ZEP_API_URL
